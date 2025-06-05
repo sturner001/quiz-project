26cbdfb9070816ddccf7beb0baf2f677bfac6e4d
Sam Turner <sam.turner@kokadjo.net>
2025-06-05 19:11:35 -0400

    implemented header, quiz components, and state, sections 193-195

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </div>
+    );
+}
 
 export default App;
